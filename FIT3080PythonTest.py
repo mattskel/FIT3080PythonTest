a8e65fdb4acaffa926bdd9f42d3ac418674bf4f7
Matthew Skelley <matthewskelley@192-168-1-3.tpgi.com.au>
2016-08-05 10:16:24 +1000

    Initial Commit

---
+++ FIT3080PythonTest.py
@@ -0,0 +1,11 @@
+# Responsible for the command line arguments
+import sys
+
+# Prints to the output in the command line
+print("Hello World")
+
+# Prints the first input after the file name
+print(sys.argv[1])
+
+# Prints the second input after the file name
+print(sys.argv[2])
